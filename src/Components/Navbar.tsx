f46e0462ad5330693eef4feb8b9d4c9f6e082a5d
Sairaj <sairaj.patil2209@gmail.com>
2025-08-30 20:25:41 +0530

    removed all NavItems from Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-interface NavItem {
-  name: string;
-  href: string;
-}
+
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
